6ea4edd9704f60676856eb6f1c399f436cc9cf13
Joe Stanley <engineerjoe440@yahoo.com>
2023-01-13 21:58:43 -0800

    cleanup of primary module

--- docsource/conf.py
+++ docsource/conf.py
@@ -18,7 +18,7 @@
 # Verify Import
 try:
     import comtrade
-except:
+except ImportError:
     print("Couldn't import `comtrade` module!")
     sys.exit(9)
 
@@ -35,12 +35,13 @@ author = 'David Rodrigues Parrini'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [  'sphinx.ext.autodoc',
-                'sphinx.ext.napoleon',
-                'sphinx.ext.autosummary',
-                'numpydoc',
-                'sphinx_sitemap',
-                'm2r2',
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
+    'numpydoc',
+    'sphinx_sitemap',
+    'm2r2',
 ]
 autosummary_generate = True
 numpydoc_show_class_members = True
@@ -90,8 +91,5 @@ html_title = 'python-comtrade'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['static']
-
-
-
 
 # END
